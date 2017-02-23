08416ceceafd33bfd3165281abf88a745d09e7f5
ntkduy <ntkduy1604@gmail.com>
2017-02-23 19:55:19 +0800

    Update findARestaurant return restaurant name, address & image

--- vagrant/APIs/findARestaurant.py
+++ vagrant/APIs/findARestaurant.py
@@ -30,19 +30,38 @@ def findARestaurant(mealType,location):
 		for i in venues_address:
 			address += i + " "
 		venues_address 	= address
+		#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
+		url = ('https://api.foursquare.com/v2/venues/%s/photos?client_id=%s&client_secret=%s&v=20170223'
+		% (venues_id,foursquare_client_id, foursquare_client_secret))
+		result = json.loads(h.request(url,'GET')[1])
+		#5. Grab the first image
+		if result['response']['photos']['items']:
+			first_photo 	= result['response']['photos']['items'][0]
+			photo_prefix	= first_photo['prefix']
+			photo_suffix	= first_photo['suffix']
+			imageURL 		= photo_prefix + "300x300" + photo_suffix
+		#6. If no image is available, insert default a image url
+		else:
+			imageURL		= "http://pixabay.com/get/8926af5eb597ca51ca4c/1433440765/cheeseburger-34314_1280.png?direct"
 		#7. Return a dictionary containing the restaurant name, address
-		restaurantInfo = {'name':venues_name, 'address':venues_address}
+		restaurantInfo = {'name':venues_name, 'address':venues_address, 'image':imageURL}
 		print "Restaurant Name: %s" 	% restaurantInfo['name']
 		print "Restaurant Address: %s" 	% restaurantInfo['address']
+		print "Image: %s \n" % restaurantInfo['image']
 		return restaurantInfo
 	else:
 		print "No Restaurants Found for %s" % location
 		return "No Restaurants Found"
-		
-	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
-	#5. Grab the first image
-	#6. If no image is available, insert default a image url
+	
 if __name__ == '__main__':
 	findARestaurant("Pizza", "Tokyo, Japan")
+	findARestaurant("Tacos", "Jakarta, Indonesia")
+	findARestaurant("Tapas", "Maputo, Mozambique")
+	findARestaurant("Falafel", "Cairo, Egypt")
+	findARestaurant("Spaghetti", "New Delhi, India")
+	findARestaurant("Cappuccino", "Geneva, Switzerland")
+	findARestaurant("Sushi", "Los Angeles, California")
+	findARestaurant("Steak", "La Paz, Bolivia")
+	findARestaurant("Gyros", "Sydney Australia")
 
 
